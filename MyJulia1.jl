3feabcca5ea239ab5ac8b0261314d6235d2f7cb5
Arun Veeramany <arun.veeramany@pnnl.gov>
2018-01-03 11:04:33 -0800

    REVAMPED Script

--- MyJulia1.jl
+++ MyJulia1.jl
@@ -1,8 +1,29 @@
+using Complementarity;
+using JuMP;
+
 include("def.jl");
 include("dProc.jl");
 include("buildMod.jl");
 
-function MyJulia1(rawFile, genFile, contFile) 
+
+#-------------------------------------------------------------------------------
+#INPUTS
+#-------------------------------------------------------------------------------
+base = "/home/svcarpacomp/data/105/Phase_0_RTS96/scenario_1/"
+base = "/data/105/Phase_0_RTS96/scenario_1/"
+#base = "/data/141982/Phase_0_IEEE14_1Scenario/scenario_1/"
+
+rawFile =base * "powersystem.raw";
+genFile =base * "generator.csv";
+contFile=base * "contingency.csv";
+#-------------------------------------------------------------------------------
+
+
+#-------------------------------------------------------------------------------
+#LOAD DATA FROM INPUT FILES
+#-------------------------------------------------------------------------------
+
+#function MyJulia1(rawFile, genFile, contFile) 
 
 # written by Haoxiang Yang, Northwestern University, 404-421-0638, haoxiangyang2019@u.northwestern.edu
 # modified by Stephen Elbert and Jesse Holzer, PNNL; Miles Lubin, Google
@@ -13,8 +34,6 @@ function MyJulia1(rawFile, genFile, contFile)
 
     println("Data generation")
 
-    tic()
-
     baseMVA,busSeg,loadSeg,shuntSeg,genSeg,branchSeg,transformerSeg = preProc(rawFile);
     busList,busDList,genList,genDList = busgenProc(baseMVA,busSeg,shuntSeg,loadSeg,genSeg,genFile);
     brList,brListSingle,brDList = branchProc(baseMVA,branchSeg,transformerSeg);
@@ -23,105 +42,167 @@ function MyJulia1(rawFile, genFile, contFile)
     fData = fixedData(baseMVA,busList,busDList,genList,genDList,brList,brListSingle,brDList);
     uData = uncertainData(contList,contDList);
 
-    toc()
-
-    println("Model generation")
-
-    tic()
     mp = buildMod(fData,uData, contDList);
-    toc()
 
-    println("Model solve")
-    tic()
-    solve(mp);
-    toc()
+  # Input:
+  # fData - grid data with all parameters
+  # uData - contingency data
+
+  # readin the data from the structure
+  baseMVA = fData.baseMVA;
+  bList = fData.busList;
+  bData = fData.busDList;
+
+  gList = fData.genList;
+  gData = fData.genDList;
+
+  brList = fData.brList;
+  brData = fData.brDList;
+
+  S = uData.contList;
+  contData = uData.contDList;
+
+  # set up the model
+   mp = Model(solver = KnitroSolver(KTR_PARAM_OUTLEV=2,  # default is 2
+   			#feastol=2.25e-9, 
+   			#opttol=1e-4, 
+   			cg_maxit=10,   # formerly maxcgit
+   			maxit=4000,
+   			#ftol=1e-4,
+        	bar_initmu=0.12,
+   			maxtime_real=3600,
+   			algorithm=5,
+   			infeastol=1e-20,
+   			#ms_enable=1,
+   			#ms_deterministic =0,
+   			#par_numthreads =3,
+   			#par_msnumthreads =3
+   			)); 
+   			
+  # create the variables for the base case
+  @variable(mp,bData[i].Vmin <= v0[i in bList] <= bData[i].Vmax);
+  @variable(mp,gData[l].Pmin <= sp0[l in gList] <= gData[l].Pmax);
+  @variable(mp,gData[l].Qmin <= sq0[l in gList] <= gData[l].Qmax);
+  @variable(mp,p0[k in brList]);
+  @variable(mp,q0[k in brList]);
+  @variable(mp,psh0[i in bList]);
+  @variable(mp,qsh0[i in bList]);
+  @variable(mp,theta0[i in bList]);
+#-------------------------------------------------------------------------------
+
+
+
+#-------------------------------------------------------------------------------
+#REMOVE CONTINGENCY ELEMENTS (BRANCHES FOR NOW) - FEASIBLE BUT INSIGNIFICANT
+#-------------------------------------------------------------------------------
+considerContingencies = true;
+
+if considerContingencies==true
+	for c in values(contDList)
+			println("Deleting branch...",c)
+			for i in 1:2 delete!( brData, [c.Loc[i]] ); end
+			for i in 1:2 deleteat!( brList,findin(brList,[c.Loc[i]] )); end
+	end
+end
+#-------------------------------------------------------------------------------
+
+
+
+#-------------------------------------------------------------------------------
+# EQNS 22, 24	OHMS LAW
+# EQNS 23, 25 	REVERSE OF ABOVE CAUSE INFEASIBILITY, UNIMPLEMENTED
+# EQ 29			THETA BOUNDS
+#-------------------------------------------------------------------------------
+@NLconstraint(mp,pFlow0[k in brList], 
+		p0[k] == 
+			(1/brData[k].tau^2) * brData[k].g* v0[brData[k].From]^2 -
+            (1/brData[k].tau)* v0[brData[k].From]*v0[brData[k].To]*
+             (
+             	brData[k].g* cos(theta0[brData[k].From] - theta0[brData[k].To] - brData[k].thetatr) +
+             	brData[k].b* sin(theta0[brData[k].From] - theta0[brData[k].To] - brData[k].thetatr)
+             )
+       );
+
+
+ @NLconstraint(mp,qFlow0[k in brList], 
+ 			q0[k] == 
+ 			-(1.0/(brData[k].tau^2))*(brData[k].b+brData[k].bc/2)*v0[brData[k].From]^2 -
+ 			 (1/brData[k].tau)* v0[brData[k].From]*v0[brData[k].To]*
+             (
+             	brData[k].g* cos(theta0[brData[k].From] - theta0[brData[k].To] - brData[k].thetatr) -
+             	brData[k].b* sin(theta0[brData[k].From] - theta0[brData[k].To] - brData[k].thetatr)
+             )
+ 			
+        );
+
+@constraint(mp,theta_c[k in brList], 
+		-pi/3.0 <= theta0[brData[k].From]-theta0[brData[k].To] <= pi/3.0);
+
+#------------------------------------------------------------------------------------------
+
+
+
+
+#------------------------------------------------------------------------------------------
+#EQNS 26-27	KIRCHOFF'S LAW
+#------------------------------------------------------------------------------------------
+@NLconstraint(mp,pBalance0[i in bList], sum(sp0[l] for l in bData[i].gen) ==  psh0[i]*v0[i]^2 + bData[i].Pd + sum(p0[k] for k in brList if (brData[k].From == i)));
+@NLconstraint(mp,qBalance0[i in bList], sum(sq0[l] for l in bData[i].gen) == -qsh0[i]*v0[i]^2 + bData[i].Qd + sum(q0[k] for k in brList if (brData[k].From == i)));
+#------------------------------------------------------------------------------------------
+
+
+
+#------------------------------------------------------------------------------------------
+#APPARENT FLOW BOUND --- CAUSES INFEASIBILITY
+#------------------------------------------------------------------------------------------
+#@NLconstraint(mp,flowBound0[k in brList],p0[k]^2 + q0[k]^2 <= brData[k].t^2);
+#------------------------------------------------------------------------------------------
+
+
+#------------------------------------------------------------------------------------------
+#SHUNTS --- CAUSES INFEASIBILITY
+#------------------------------------------------------------------------------------------
+#@NLconstraint(mp,pShunt0[i in bList],psh0[i] == bData[i].gsh*v0[i]^2);
+#@NLconstraint(mp,qShunt0[i in bList],qsh0[i] == -bData[i].bsh*v0[i]^2);
+#------------------------------------------------------------------------------------------
+
+
+#------------------------------------------------------------------------------------------
+#COMPLEMENTARITY ---- FEASIBLE BUT INSIGNIFICANT 
+#------------------------------------------------------------------------------------------
+for i in gList
+  @complements(mp,v0[i[1]], gData[i].Qmin <= sq0[i] <= gData[i].Qmax);
+end
+#------------------------------------------------------------------------------------------
+
+
+
+#------------------------------------------------------------------------------------------
+# OBJECTIVE FUNCTION
+#------------------------------------------------------------------------------------------
+@variable(mp, CF); #STE
+@NLconstraint(mp, CF == sum(sum(gData[l].cParams[n]*(sp0[l]*fData.baseMVA)^n for n in gData[l].cn ) for l in gList if gData[l].Pmin<gData[l].Pmax)     #STE
+                       + sum(sum(gData[l].cParams[n]*(gData[l].Pmin*fData.baseMVA)^n for n in gData[l].cn ) for l in gList if gData[l].Pmin==gData[l].Pmax) 
+                        ); 
+@NLobjective(mp, Min, CF);  #STE needed to avoid error for RTS96
+#------------------------------------------------------------------------------------------
+
+
+
+#------------------------------------------------------------------------------------------
+#SOLVE THE OPTIMIZATION PROBLEM
+#------------------------------------------------------------------------------------------
+ solve(mp);
+#------------------------------------------------------------------------------------------
+
+
+#------------------------------------------------------------------------------------------
+#EXTRACT
+#------------------------------------------------------------------------------------------
     sphat = getvalue(mp[:sp0]);
     sqhat = getvalue(mp[:sq0]);
-    spshat = getvalue(mp[:sp]);
-    sqshat = getvalue(mp[:sq]);
+    #spshat = getvalue(mp[:sp]);
+    #sqshat = getvalue(mp[:sq]);
     costhat = getobjectivevalue(mp);
-
-    println("Solution writing")
-    tic()
-
-    open("solution1.txt","w") do f
-      write(f, "--generation dispatch \nbus id,unit id,pg(MW),qg(MVar) \n");
-      for i in fData.genList
-        loc = fData.genDList[i].Loc;
-        name = fData.genDList[i].Name;
-        spTemp = sphat[i]*fData.baseMVA;
-        sqTemp = sqhat[i]*fData.baseMVA;
-        write(f, "$loc,$name,$spTemp,$sqTemp \n");
-      end
-      write(f,"--end of generation dispatch \n");
-    end
-
-    open("solution2.txt","w") do f
-      write(f, "--contingency generator \nconID,genID,busID,unitID,q(MW) \n");
-      for s in uData.contList
-        counter = 0;
-        for i in fData.genList
-          counter += 1;
-          loc = fData.genDList[i].Loc;
-          idTemp = fData.genDList[i].ID;
-          name = fData.genDList[i].Name;
-          sqTemp = sqshat[i,s]*fData.baseMVA;
-          write(f,"$s,l_$counter,$loc,$name,$sqTemp \n");
-        end
-      end
-      write(f,"--end of contingency generator \n--bus \ncontingency id,bus id,v(pu),theta(deg) \n");
-      for i in fData.busList
-        id = fData.busDList[i].ID;
-        name = fData.busDList[i].Name;
-        vTemp = getvalue(mp[:v0][i]);
-        thetaTemp = getvalue(mp[:theta0][i]/pi*180);
-        write(f,"0,$id,$vTemp,$thetaTemp \n");
-      end
-      for s in uData.contList
-        for i in fData.busList
-          id = fData.busDList[i].ID;
-          name = fData.busDList[i].Name;
-          vTemp = getvalue(mp[:v][i,s]);
-          thetaTemp = getvalue(mp[:theta][i,s]/pi*180);
-          write(f,"$s,$id,$vTemp,$thetaTemp \n");
-        end
-      end
-      write(f,"--end of bus \n--Delta \ncontingency id,Delta(MW) \n");
-      for s in uData.contList
-        pdeltaTemp = getvalue(mp[:pdelta][s])*fData.baseMVA;
-        write(f,"$s,$pdeltaTemp \n");
-      end
-      write(f,"--end of Delta \n--line flow \ncontingency id,line id,origin bus id,destination bus id,circuit id,p_origin(MW),q_origin(MVar),p_destination(MW),q_destination(MVar) \n");
-      for i in fData.brListSingle
-        idTemp = fData.brDList[i].ID;
-        revidTemp = fData.brDList[i].revID;
-        fromTemp = fData.brDList[i].From;
-        toTemp = fData.brDList[i].To;
-        name = fData.brDList[i].CKT;
-        pTemp = getvalue(mp[:p0][i])*fData.baseMVA;
-        qTemp = getvalue(mp[:q0][i])*fData.baseMVA;
-        pRevTemp = getvalue(mp[:p0][revidTemp])*fData.baseMVA;
-        qRevTemp = getvalue(mp[:q0][revidTemp])*fData.baseMVA;
-        write(f,"0,$name,$fromTemp,$toTemp,i_$(fromTemp)_$(toTemp)_$(name),$pTemp,$qTemp,$pRevTemp,$qRevTemp \n");
-      end
-      for s in uData.contList
-        for i in fData.brListSingle
-          if !(i in contDList[s].Loc)
-            idTemp = fData.brDList[i].ID;
-            revidTemp = fData.brDList[i].revID;
-            fromTemp = fData.brDList[i].From;
-            toTemp = fData.brDList[i].To;
-            name = fData.brDList[i].CKT;
-            pTemp = getvalue(mp[:p][i,s])*fData.baseMVA;
-            qTemp = getvalue(mp[:q][i,s])*fData.baseMVA;
-            pRevTemp = 0.*getvalue(mp[:p][revidTemp,s])*fData.baseMVA/0.;
-            qRevTemp = 0.*getvalue(mp[:q][revidTemp,s])*fData.baseMVA/0.;
-            write(f,"$s,$name,$fromTemp,$toTemp,i_$(fromTemp)_$(toTemp)_$(name),$pTemp,$qTemp,$pRevTemp,$qRevTemp \n");
-          end
-        end
-      end
-      write(f,"--end of line flow \n")
-    end
-
-    toc()
-end
+#------------------------------------------------------------------------------------------
+    
